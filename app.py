0a1d74492db1d062b973542db6f60c51921355c7
Shivank19 <shivank.sapra@gmail.com>
2021-12-01 02:55:43 +0530

    functionality update

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
+import requests
+from requests.api import head
 
+app = Flask(__name__)
+@app.route('/', methods = ["GET","POST"])
 
+def index():
+#Scraping Algorithm
+
+    url = "https://www.businesstoday.in/technology/news"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, "html.parser")
+
+    outerdata = soup.find_all("div", class_ = "widget-listing", limit = 10)
+    headlines = ""
+    for data in outerdata:
+        news = data.div.div.a["title"]
+        headlines += "\u2022 " + news + "\n"
+    
+    return render_template("index.html", News = headlines)
+
+#Scraping Algo End
